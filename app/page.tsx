9021c515000e0212967affb61f213405af382d0a
DiligenceGo <dev@local>
2026-02-12 18:16:33 -0300

    update-pages-deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import { redirect } from "next/navigation";
+"use client";
+import { useEffect } from "react";
 
 export default function Home() {
-  redirect("/dashboard");
+  useEffect(() => {
+    window.location.assign("dashboard/");
+  }, []);
+  return null;
 }
-
